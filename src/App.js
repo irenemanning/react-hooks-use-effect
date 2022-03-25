b2e4b47cddc788f27c563746d2a6fe4bcdeb0893
Irene <irenemanningm@gmail.com>
2022-03-24 19:21:10 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }, []);
+  let text = "Poop"
+  useEffect(() => {
+    
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
